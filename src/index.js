e1cbb10710a7dd17993548b9a3d90cacd8d502ea
htkha7166 <huynhkha2601@gmail.com>
2022-12-19 15:53:46 +0700

    customer home
    - topbar
    - sidebar
    - haven't implement styled

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
+import { ProSidebarProvider } from 'react-pro-sidebar';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root'),
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ProSidebarProvider>
+        <App />
+      </ProSidebarProvider>
     </BrowserRouter>
   </Provider>,
   // </React.StrictMode>
